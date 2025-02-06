9d92800f656b4eb29da73f19d490c2233f834d23
Pravat <itspravatorg@gmail.com>
2025-02-06 22:37:05 +0530

    Delete ass5 directory

--- ass5/MFR/usingMap.js
+++
@@ -1,9 +0,0 @@
-const products = [
-    { name: "Laptop", price: 800, category: "Electronics" },
-    { name: "Chair", price: 120, category: "Furniture" },
-    { name: "Headphones", price: 150, category: "Electronics" },
-    { name: "Table", price: 200, category: "Furniture" }
-  ];
-  
-  const productNamesUppercase = products.map(product => product.name.toUpperCase());
-  console.log(productNamesUppercase);
